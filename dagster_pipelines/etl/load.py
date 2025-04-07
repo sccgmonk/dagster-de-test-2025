f8d0735583f6c417bd65d5cde576c39d7e6a1a79
sccgmonk <158587003+sccgmonk@users.noreply.github.com>
2025-04-07 12:20:27 +0700

    main add db

--- dagster_pipelines/etl/load.py
+++ dagster_pipelines/etl/load.py
@@ -7,14 +7,16 @@ logger = get_dagster_logger()
 def load_to_duckdb(df: pd.DataFrame, table_name: str) -> None:
     try:
         with duckdb.connect("/opt/dagster/app/dagster_pipelines/db/plan.db") as con:
+
             logger.info("Connected to DuckDB successfully.")
             
             con.sql("CREATE SCHEMA IF NOT EXISTS plan;")
             con.register('df_view', df)
-            con.execute(f"INSERT INTO {table_name} SELECT * FROM df;")
             
             con.sql(f"CREATE OR REPLACE TABLE plan.plan.{table_name} AS SELECT * FROM df_view;")
-            result = con.sql(f"SELECT * FROM plan.plan.{table_name} LIMIT 1").fetchone()
+            con.execute(f"INSERT INTO  plan.plan.{table_name} SELECT * FROM df;")
+
+            result = con.sql(f"SELECT * FROM plan. plan.{table_name} LIMIT 1").fetchone()
             logger.info(f"Sample record from {table_name}: {result}")
             logger.info(f"Data successfully inserted into table '{table_name}'.")
     except Exception as e:
